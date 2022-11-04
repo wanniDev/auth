ccbfd711850b55d28fc8b0a231944e35b7bb6b92
wanniDev <wannidev0928@gmail.com>
2022-11-05 00:19:42 +0900

    Feat: 스프링 시큐리티 기본설정 적용
    - 인증과정에서 쿠키를 사용하지 않기 때문에, csrf 는 비활성화
    - api 요청 자체로 인증을 진행할 것이므로, 스프링 시큐리티가 제공하는 formLogin 기능은 비활성화
    - 세션인증을 진행하진 않을 것이기 때문에 세션 관리 정책은 무상태로 변경

---
+++ src/main/kotlin/me/spring/auth/account/infrastructure/security/SecurityConfig.kt
@@ -0,0 +1,29 @@
+package me.spring.auth.account.infrastructure.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+class SecurityConfig {
+    @Bean
+    fun filterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity
+            .csrf()
+                .disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .mvcMatchers("/api/v1/account/auth", "/api/v1/account/join")
+                .permitAll()
+                .and()
+            .formLogin()
+                .disable()
+            .build()
+    }
+}
