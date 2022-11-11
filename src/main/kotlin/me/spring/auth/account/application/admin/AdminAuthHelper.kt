a46e18e01848681d4f5c11dac61f57080d77c959
wanniDev <wannidev0928@gmail.com>
2022-11-11 16:04:58 +0900

    Feat: admin 로그아웃 추가
    - 물리적으로는 같은 코드지만.. 논리적으로는 admin 로그아웃을 분리한 상황이기에 코드가 같아도 따로 구현하였다.
    - 시큐어 코딩일 필요할 경우, 인증 메세지 자체를 따로 관리해야할 수도 있으므로, 우선은 서로 다른 helper를 의존하는 것을 유지하였다.

--- src/main/kotlin/me/spring/auth/account/application/admin/AdminAuthHelper.kt
+++ src/main/kotlin/me/spring/auth/account/application/admin/AdminAuthHelper.kt
@@ -1,9 +1,12 @@
 package me.spring.auth.account.application.admin
 
+import me.spring.auth.account.application.AuthBlockManager
 import me.spring.auth.account.application.AuthHelper
 import me.spring.auth.account.application.AuthToken
+import me.spring.auth.account.application.email.EmailMessage
 import me.spring.auth.account.domain.Account
 import me.spring.auth.account.infrastructure.security.JwtAuthenticationToken
+import me.spring.auth.exception.NotFoundAccountException
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.core.context.SecurityContextHolder
@@ -12,7 +15,10 @@ import org.springframework.stereotype.Component
 
 @Component
 @Qualifier("adminAuthHelper")
-class AdminAuthHelper(@Qualifier("adminAuthenticationManager") private val authenticationManager: AuthenticationManager, private val passwordEncoder: PasswordEncoder) : AuthHelper {
+class AdminAuthHelper(@Qualifier("adminAuthenticationManager") private val authenticationManager: AuthenticationManager,
+                      private val passwordEncoder: PasswordEncoder,
+                      private val authBlockManager: AuthBlockManager<Long>
+) : AuthHelper {
     override fun authenticate(account: Account): AuthToken {
         val authToken = JwtAuthenticationToken(account, "")
         val authenticate = authenticationManager.authenticate(authToken)
@@ -25,6 +31,13 @@ class AdminAuthHelper(@Qualifier("adminAuthenticationManager") private val authe
     }
 
     override fun invalidate(id: Long): Boolean {
+        if(!authBlockManager.existsById(id)) {
+            return authBlockManager.add(id)
+        }
+        throw NotFoundAccountException("Already invalidated token...")
+    }
+
+    override fun validateMail(emailMessage: EmailMessage) {
         TODO("Not yet implemented")
     }
 }
