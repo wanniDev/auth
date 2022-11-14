03ec07fe8e9651393088e4f574f9e282229524a8
wanniDev <wannidev0928@gmail.com>
2022-11-14 17:22:25 +0900

    Feat: 이메일 검증 api 코드 추가(받아서 검증하는 로직은 우선 콘솔에 프린트하여 전달여부만 확인)

---
+++ src/main/kotlin/me/spring/auth/account/presentation/EmailApi.kt
@@ -0,0 +1,23 @@
+package me.spring.auth.account.presentation
+
+import me.spring.auth.account.application.email.EmailFacade
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api")
+class EmailApi(private val emailFacade: EmailFacade) {
+    @PostMapping("v1/email/valid")
+    fun sendEmail(@RequestBody emailReq: Map<String, String>): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(emailFacade.sendValidation(emailReq))
+    }
+
+    @GetMapping("v1/email/valid")
+    fun checkEmail(email: String, token: String) {
+        println("email: $email, token: $token")
+    }
+}
